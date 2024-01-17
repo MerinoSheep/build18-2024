d80d915b036f7947f18a3994df4df96894336a6a
Liam Merino <35940408+MerinoSheep@users.noreply.github.com>
2024-01-17 18:57:02 -0500

    letter selection

--- python/read.py
+++ python/read.py
@@ -4,16 +4,20 @@ import readchar
 import msvcrt # Windows only, for now
 ser = serial.Serial("COM3",9600)
 isRecording = False
+letter = '?'
 if os.name == 'nt':
     os.system('cls')
 else:
     os.system('clear')
 
-def recordingCheck():
+def keypress_check():
     global isRecording
+    global letter
     if msvcrt.kbhit():
-        if str(msvcrt.getch()) == "b's'":
+        if bytes.decode(msvcrt.getch()) == '1':
             isRecording = not isRecording;
+        if bytes.decode(msvcrt.getch()).isalpha():
+            letter = bytes.decode(msvcrt.getch()).upper()
 # Colors
 RED = '\033[31m'
 GREEN = '\033[32m'
@@ -25,10 +29,14 @@ RESET = '\033[0m' # reset to the default color
 
 LINE_UP = '\033[1A'
 CLEAR = "\033[K"
+
+if not os.path.exists('data'):
+    os.makedirs('data')
+
 while True:
     # print('\033[?25l', end="")
     line = ser.readline()
-    recordingCheck()
+    keypress_check()
     if line:
         line = line.decode("utf-8").rstrip("\r\n");
         values = line.split(',');
@@ -38,8 +46,16 @@ while True:
         print(f"{CLEAR}Sensor Three:{values[2]}", end='\n')
         print(f"{CLEAR}Sensor Four:{values[3]}", end='\n')
         print(f"{CLEAR}Sensor Five:{values[4]}", end='\n')
+        print(f"{CLEAR}Sensor Six:{values[5]}", end='\n')
+        print(f"{CLEAR}Sensor Seven:{values[6]}", end='\n')
+        print(f"{CLEAR}Sensor Eight:{values[7]}", end='\n')
+        print(f"{CLEAR}Sensor Nine:{values[8]}", end='\n')
+        print(f"{CLEAR}Sensor Ten:{values[9]}", end='\n')
+        print(f"{CLEAR}Sensor Eleven:{values[10]}", end='\n')
         if not isRecording:
-            print(f"{CLEAR}{GREEN}[s]tart recording{RESET}", end='\n')
+            print(f"{CLEAR}{GREEN}[1] start recording{RESET}", end='')
         else:
-            print(f"{CLEAR}{RED}[s]top recording{RESET}", end='\n')
+            print(f"{CLEAR}{RED}[1] stop recording{RESET}", end='')
+        print(f"\t\tCurrent Letter:{BLUE}{letter}{RESET}", end = "\n")
+
         print(LINE_UP*8, end='')
