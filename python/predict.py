266f7c41bfa3b83b556f8fc8b75a4d4f3d455a97
Liam Merino <35940408+MerinoSheep@users.noreply.github.com>
2024-01-20 13:40:13 -0500

    prediction python script

---
+++ python/predict.py
@@ -0,0 +1,33 @@
+import os
+import numpy as np
+import serial
+import tensorflow as tf
+from tensorflow import keras
+import msvcrt # Windows only, for now
+
+
+ser = serial.Serial("COM4",9600)
+print(tf.version.VERSION)
+
+model = tf.keras.models.load_model("my_model.h5")
+
+arr = np.zeros.rand(1,1000)
+
+while True:
+    msvcrt.getch()
+    for i in range(100):
+        line = ser.readline()
+        line = line.decode("utf-8").rstrip("\r\n");
+        values = line.split(',');
+        arr[i+0] = values[0]
+        arr[i+1] = values[1]
+        arr[i+2] = values[2]
+        arr[i+3] = values[3]
+        arr[i+4] = values[4]
+        arr[i+5] = values[5]
+        arr[i+6] = values[6]
+        arr[i+7] = values[7]
+        arr[i+8] = values[8]
+        arr[i+9] = values[9]
+
+    print(model.predict(arr))
