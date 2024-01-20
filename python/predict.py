eec6d0421238a6011cafd70e3da87b5ef1fa1508
Liam Merino <35940408+MerinoSheep@users.noreply.github.com>
2024-01-20 17:54:09 -0500

    new model

--- python/predict.py
+++ python/predict.py
@@ -35,27 +35,36 @@ GESTURES = [
 ]
 
 ser = serial.Serial("COM4",9600)
-print(tf.version.VERSION)
+# print(tf.version.VERSION)
 
-model = tf.keras.models.load_model("my_model.h5")
-
-arr = np.zeros.rand(1,1000)
+print("Loading model...")
+model = tf.keras.models.load_model("./my_model.keras")
+print("Model loaded!")
+arr = np.zeros((1,1000))
 
 while True:
+    print("Waiting for keypress...")
     msvcrt.getch()
+    print("Keypress detected!")
     for i in range(100):
+        print(i)
         line = ser.readline()
         line = line.decode("utf-8").rstrip("\r\n");
         values = line.split(',');
-        arr[i+0] = values[0]
-        arr[i+1] = values[1]
-        arr[i+2] = values[2]
-        arr[i+3] = values[3]
-        arr[i+4] = values[4]
-        arr[i+5] = values[5]
-        arr[i+6] = values[6]
-        arr[i+7] = values[7]
-        arr[i+8] = values[8]
-        arr[i+9] = values[9]
+        print(values)
+        # arr[i+0] = values[0]
+        # arr[i+1] = values[1]
+        # arr[i+2] = values[2]
+        # arr[i+3] = values[3]
+        # arr[i+4] = values[4]
+        # arr[i+5] = values[5]
+        # arr[i+6] = values[6]
+        # arr[i+7] = values[7]
+        # arr[i+8] = values[8]
+        # arr[i+9] = values[9]
+        for j in range(10):  # Assuming values has 10 elements
+            arr[0, i*10+j] = values[j]
 
     result = model.predict(arr)
+    print(result)
+    # print(GESTURES[np.argmax(result)])
