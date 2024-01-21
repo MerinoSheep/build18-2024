07d50ff48fd390e3a28bc03723bb04b199946582
Liam Merino <35940408+MerinoSheep@users.noreply.github.com>
2024-01-20 20:43:51 -0500

    nn

--- python/predict.py
+++ python/predict.py
@@ -4,6 +4,7 @@ import serial
 import tensorflow as tf
 from tensorflow import keras
 import msvcrt # Windows only, for now
+import time
 
 GESTURES = [
     "A",
@@ -35,10 +36,10 @@ GESTURES = [
 ]
 
 ser = serial.Serial("COM4",9600)
-# print(tf.version.VERSION)
+print(tf.version.VERSION)
 
 print("Loading model...")
-model = tf.keras.models.load_model("./my_model.keras")
+model = tf.keras.models.load_model("my_model.keras")
 print("Model loaded!")
 arr = np.zeros((1,1000))
 
@@ -46,25 +47,30 @@ while True:
     print("Waiting for keypress...")
     msvcrt.getch()
     print("Keypress detected!")
+    ser.reset_input_buffer()
     for i in range(100):
-        print(i)
         line = ser.readline()
+        if not line:
+            print("No line detected!")
+            break
         line = line.decode("utf-8").rstrip("\r\n");
         values = line.split(',');
+        values = list(map(int, values))
         print(values)
-        # arr[i+0] = values[0]
-        # arr[i+1] = values[1]
-        # arr[i+2] = values[2]
-        # arr[i+3] = values[3]
-        # arr[i+4] = values[4]
-        # arr[i+5] = values[5]
-        # arr[i+6] = values[6]
-        # arr[i+7] = values[7]
-        # arr[i+8] = values[8]
-        # arr[i+9] = values[9]
-        for j in range(10):  # Assuming values has 10 elements
-            arr[0, i*10+j] = values[j]
-
+        #ax, ay, az, gx, gy, gz, r1, r2, r3, r4 from arduino
+        arr[0][i*10+0] = (values[0]/8192+4) / 8
+        arr[0][i*10+1] = (values[1]/8192+4) / 8
+        arr[0][i*10+2] = (values[2]/8192+4) / 8
+        arr[0][i*10+3] =  (values[3] /16.384+2000) / 4000
+        arr[0][i*10+4] =  (values[4] /16.384+2000) / 4000
+        arr[0][i*10+5] =  (values[5] /16.384+2000) / 4000
+        arr[0][i*10+6] = values[6] / 1023
+        arr[0][i*10+7] = values[7] / 1023
+        arr[0][i*10+8] = values[8] / 1023
+        arr[0][i*10+9] = values[9] / 1023
+        # for j in range(10):  # Assuming values has 10 elements
+        #     arr[0, i*10+j] = values[j]
+    print(np.round(arr, 2))
     result = model.predict(arr)
-    print(result)
-    # print(GESTURES[np.argmax(result)])
+    # print(result)
+    print(GESTURES[np.argmax(result)])
